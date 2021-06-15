1308c4e6488186272fb1872e20afef804ae54bbd
scottCarson33 <48042385+scottCarson33@users.noreply.github.com>
2021-06-15 00:02:20 -0400

    fully functional, needs some testing and some prettying

--- routes/HVACData.js
+++ routes/HVACData.js
@@ -4,39 +4,71 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const moment  = require('moment')
 
-getMinAndMaxTemps = async(startDate, endDate, filePath) =>{
-    let data = {}
- await fs.createReadStream(filePath)
+async function getHVACData (startDate, endDate, filePath) {
+  const ACTemp = 75
+  const heatTemp = 62
+
+  let data = {}
+  return new Promise(function(resolve, reject) {
+ fs.createReadStream(filePath)
   .pipe(csv())
   .on('data', (row) => {
       let dateTime = moment(row['Date time'], "MM/DD/YYYY HH:mm:ss")
-      let day = moment(row['Date time'], "MM/DD/YYYY")
+      let day = moment(row['Date time'], "MM/DD/YYYY").format("MM/DD/YYYY")
     if (startDate <= dateTime&& dateTime <= endDate) {
         if(data[day] == null){
             data[day] = {}
+            data[day].heating = false
+            data[day].airConditioning = false
+
         }
-        if(data[day].min == null || data[day].min > row['Minimum Temperature']){
-            data[day].min = row['Minimum Temperature']
+        if(!data[day].heating) {
+            if(row['Minimum Temperature'] < heatTemp){
+              data[day].heating = true
+            }
         }
-        if(data[day].max == null || data[day].max < row['Maximum Temperature']){
-            data[day].max = row['Maximum Temperature']
-        }
+        if(!data[day].airConditioning) {
+          if(row['Maximum Temperature'] > ACTemp){
+            data[day].airConditioning = true
+          }
+      }
         
     }
   })
   .on('end', () => {
-    return data
+     resolve(data)
   });
+})
 
 }
 
-router.get('/monthly/:month', async (req, res) => {
+//send request by supplying Month 
 
+router.get('/monthlySummary/:month', async(req, res) => {
+try{
 const startDate = moment(req.params.month, "MM-DD-YYYY").startOf('month')
 const endDate = moment(startDate).endOf('month')
-let data = await getMinAndMaxTemps(startDate, endDate, './history_data_hourly.csv')
- res.json({test:data})
+const HVACData  = await getHVACData(startDate, endDate, './history_data_hourly.csv')
+res.status(200).send({HVACData})
+}catch(err){
+  console.log(err)
+  res.status(400).send({error: "Uh Oh Something went wrong"})
+}
 })
+
+
+//send request by giving date range
+router.get('/dateRangeSummary/:startDate/:endDate', async(req, res) => {
+  try{
+  const startDate = moment.unix(req.params.startDate)
+  const endDate = moment.unix(req.params.endDate)
+  const HVACData  = await getHVACData(startDate, endDate, './history_data_hourly.csv')
+  res.status(200).send({HVACData})
+  }catch(err){
+    console.log(err)
+    res.status(400).send({error: "Uh Oh Something went wrong"})
+  }
+  })
 
 
 
--- test.js
+++
@@ -1,2 +0,0 @@
-const moment  = require('moment')
-console.log(moment("06-01-2020", "MM-DD-YYYY"))
